7db5488840a5cb45860248249d57679b505f4edd
Fabrice David Nemo <fabrice.nemo@epfl.ch>
2023-11-19 19:39:53 +0100

    vote

--- bot.py
+++ bot.py
@@ -1,24 +1,62 @@
 import asyncio
 import json
 import nextcord as discord
-from nextcord.ext import commands
+from nextcord.ext import commands, tasks
 import os
 import re
 
-from constantes import token, prefix
+from arrow import utcnow
+from typing import Dict, List, Tuple
+from constantes import token, prefix, mainAdminId
+from vote import Vote, condorcet
 
 #info in json
 if "bot_info.json" in os.listdir(os.path.dirname(__file__)):
     with open("bot_info.json", "r") as f:
         info = json.load(f)
 else:
-    info = {"smart_tweet": dict()}
+    info = {"smart_tweet": dict(), "votes": dict()}
     with open("bot_info.json", "w") as f:
         json.dump(info, f)
 
 def save():
     with open("bot_info.json", "w") as f:
         json.dump(info, f)
+
+for categ in ("smart_tweet", "votes"):
+    if categ not in info:
+        info[categ] = dict()
+        save()
+
+async def dmChannelUser(user):
+    """
+    Récupérer le DM Channel d'un utilisateur donné
+    """
+    
+    if user.dm_channel is None:
+        await user.create_dm()
+    return user.dm_channel
+
+async def traitementRawReact(payload):
+    """
+    Récupérer les infos intéressantes d'une réaction discord
+    """
+
+    if payload.user_id != bot.user.id: #sinon, on est dans le cas d'une réaction en dm
+        messageId = payload.message_id
+        guild = bot.get_guild(payload.guild_id) if payload.guild_id else None
+        try:
+            user = (await guild.fetch_member(payload.user_id)) if guild else (await bot.fetch_user(payload.user_id))
+        except:
+            user = (await bot.fetch_user(payload.user_id))
+        channel = await bot.fetch_channel(payload.channel_id)
+
+        partEmoji = payload.emoji
+        emojiHash = partEmoji.id if partEmoji.is_custom_emoji() else partEmoji.name
+
+        return locals()
+    else:
+        return None
 
 async def smart_tweet(msg: discord.Message, delete: bool = False):
     """
@@ -50,9 +88,89 @@ async def smart_tweet(msg: discord.Message, delete: bool = False):
         msgRep = await msg.channel.fetch_message(infoSmartTweet[msgId])
         await msgRep.edit(content = ".")
 
+async def organise_vote(channel: discord.TextChannel, options: List[str]):
+    if "votes" not in info:
+        info["votes"]: Dict[str, Tuple[List[str], Dict[str, List[str]]], Tuple[int, int, int, int, int], Tuple[int, int, int, int, int], int] = dict()
+
+    msgVote = await channel.send("**Vous pouvez voter jusqu'à mardi 21 novembre 20h**\nOptions en lice :\n" + "\n".join("- " + x for x in options) + "\n\nRéagissez avec ✅ pour voter")
+    await msgVote.add_reaction("✅")
+
+    info["votes"][str(msgVote.id)] = (options, dict(), (2023, 11, 21, 20, 0), channel.id)
+
+    save()
+
+async def enregistre_vote(messageId, user, emojiHash):
+    if "votes" not in info or str(messageId) not in info["votes"]:
+        return
+    
+    if emojiHash == "✅":
+        options, recupVotes, _, _ = info["votes"][str(messageId)]
+        vue = Vote(options, recupVotes, save)
+
+        dmChannel = await dmChannelUser(user)
+        await dmChannel.send("Choisis ton option **préférée**", view = vue)
+
+async def depouillement(bot, msgId):
+    options, votes, _, channelId = info["votes"][str(msgId)]
+
+    channel = await bot.fetch_channel(int(channelId))
+    gagnant, details = condorcet(votes, options)
+
+    await channel.send(f"Le vote a été remporté par {gagnant} !")
+
+    infoVote = []
+    printF = lambda *args, end = "\n": infoVote.append(" ".join(str(x) for x in args) + end)
+    printF("Résultats")
+    
+    #full rankings per voter
+    names: Dict[int, str] = dict() #{user_id: user_name}
+    for voterId, vote in votes.items():
+        if voterId not in names:
+            names[voterId] = (await bot.fetch_user(voterId)).name
+
+        printF(f"{names[voterId]}:", ", ".join(vote))
+
+    detailsProcessed: Dict[str, List[Tuple[str, int, int]]] = {sub: [] for sub in options}
+
+    for (winner, loser), (pointsWinner, pointsLoser) in details.items():
+        detailsProcessed[winner].append((loser, pointsWinner, pointsLoser))
+    
+    affiResDuels = lambda sub: "\n".join(f"a gagné le duel contre {loser} ({pointsWinner}-{pointsLoser})" for loser, pointsWinner, pointsLoser in detailsProcessed[sub])
+
+    printF()
+
+    for sub in options:
+        isWinner = "a gagné le vote" if sub == gagnant else ""
+        printF(f"{sub} {isWinner}\n" + affiResDuels(sub))
+        printF()
+    printF()
+    
+    with open(f"resultats_vote_{msgId}.txt", "w") as f:
+        f.write("".join(infoVote))
+    
+    await channel.send("Résultats détaillés:", file = discord.File(f"resultats_vote_{msgId}.txt", filename="resultats.txt"))
+
+async def planning(now, bot):
+    datetime = (now.year, now.month, now.day, now.hour, now.minute)
+
+    #votes
+    if "votes" in info:
+        for msgId, voteInfo in info["votes"].items():
+            if tuple(voteInfo[2]) == datetime:
+                await depouillement(bot, msgId)
+
 def main():
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix=prefix, help_command=None, intents = intents)
+
+    @tasks.loop(minutes = 1.0)
+    async def autoplanner():
+        now = utcnow().to("Europe/Brussels")
+        await planning(now, bot)
+
+    @bot.event
+    async def on_ready():
+        autoplanner.start()
 
     @bot.event
     async def on_message(message):
@@ -66,6 +184,20 @@ def main():
     @bot.event
     async def on_message_delete(message):
         await smart_tweet(message, delete=True)
+
+    @bot.event
+    async def on_raw_reaction_add(payload):
+        traitement = await traitementRawReact(payload)
+        if traitement:
+            messageId = traitement["messageId"]
+            user = traitement["user"]
+            if user.bot: return #no need to go further
+
+            guild = traitement["guild"]
+            emojiHash = traitement["emojiHash"]
+            channel = traitement["channel"]
+        
+            await enregistre_vote(messageId, user, emojiHash)
     
     @bot.command(name = "ursula")
     async def ursula_command(ctx):
@@ -75,6 +207,12 @@ def main():
 
         await ctx.send(embed = e, reference = ref)
     
+    @bot.command(name = "vote")
+    async def command_vote(ctx):
+        if ctx.author.id == mainAdminId:
+            options = ["Ouip", "Nhop", "Switch", "France Chômage", "Le Fouet"]
+            await organise_vote(ctx.channel, options)
+
     return bot, token
 
 if __name__ == "__main__": #pour lancer le bot
---
+++ vote.py
@@ -0,0 +1,130 @@
+from typing import Dict, List, Callable, Tuple
+import nextcord as discord
+
+UserId = str
+Bulletin = List[str] #liste ordonnée
+
+def Vote(options: List[str], votes: Dict[UserId, Bulletin], saveData: Callable[[], None]):
+    """
+    Definis une vue discord pour un vote
+    """
+
+    class Aux(discord.ui.View):
+        def __init__(self):
+            super().__init__(timeout = 3600)
+            self.selectedItems: List[str] = []
+        
+        def showSelected(self):
+            selectedItems = self.selectedItems
+            return "\n".join(f"**#{i+1}** {affi}" for i, affi in enumerate(selectedItems))
+
+    Aux.__view_children_items__ = []
+
+    for opt in options:
+        def aux(opt: str):
+            @discord.ui.button(label = opt)
+            async def callback(self, button: discord.ui.Button, interaction: discord.Interaction):
+                self.selectedItems.append(opt)
+                button.disabled = True
+
+                if len(self.selectedItems) < len(options) - 1:
+                    await interaction.message.edit(content = self.showSelected() + "\n" + f"Choisis ta **{len(self.selectedItems)+1}e option préférée** (le vote n'est valide que si toutes les options ont été classées)", view=self)
+                else:
+                    vote = self.selectedItems
+                    #on a sélectionné toutes les options sauf une, il faut retrouver laquelle
+                    vote.extend([x for x in options if x not in vote])
+
+                    for child in self.children: #on désactive tous les boutons
+                        child.disabled = True
+
+                    votes[str(interaction.user.id)] = vote
+                    saveData()
+
+                    await interaction.message.edit(content = "**Ton vote a été enregistré**\nTu peux changer ton vote en réagissant de nouveau sur le serveur.\n\n" + self.showSelected(), view = self)
+            
+            return callback
+        
+        fonc = aux(opt)
+        
+        #add the button to Aux
+        setattr(Aux, f"callback_{opt}", fonc)
+        Aux.__view_children_items__.append(fonc)
+    
+    return Aux()
+
+def condorcet(classements: Dict[int, List[str]], options: List[str]) -> Tuple[str, Dict[Tuple[str, str], Tuple[int, int]]]:
+    """
+    Compute the results of each duel from ranked voting ballots.
+    Returns the Condorcet winner (or None if there is no winner) and detailed duel results.
+
+    Args:
+    - classements, dictionary {voter_id: [submissions_ranked_by_voter]}
+    - submissions, list
+
+    Returns:
+    - the Condorcet winner (if it exists, the Borda_elim winner otherwise), str
+    - detailed duel results, Dict[Tuple[str, str], Tuple[str, Tuple[int, int]]]
+    """
+
+    def borda_elim():
+        optionsLoc = set(tuple(x) for x in options)
+        while len(optionsLoc) > 1:
+            nbPoints = {c: 0 for c in optionsLoc}
+
+            for ranking in classements.values():
+                for i, sub in enumerate(filter(lambda x: x in nbPoints, ranking)):
+                    nbPoints[sub] += len(optionsLoc) - i
+            
+            loser = min(nbPoints.items(), key=lambda x: (x[1], -x[0][2]))[0]
+            #we remove the submission that got the lowest amount of points
+            #in case of a tie, the submission that got submitted later gets the priority for getting removed
+            
+            optionsLoc.remove(loser)
+        
+        return optionsLoc.pop()
+
+    if classements == dict():
+        #on prend la première option
+        return options[0], dict()
+    else:
+        #{candidate_1: {candidate2: number_votes_candidate1_preferred_over_candidate2}}
+        countsDuels: Dict[str, Dict[str, float]] = {c: {c2: 0 for c2 in options if c != c2} for c in options}
+
+        for vote in classements.values():
+            for i, subI in enumerate(vote):
+
+                for j in range(i+1, len(vote)):
+                    countsDuels[subI][vote[j]] += 1
+        
+        #{(winner, loser): (score_winner, score_loser)}
+        winsDuels: Dict[Tuple[str, str], Tuple[float, float]] = dict()
+        for i, subI in enumerate(options):
+            for j in range(i+1, len(options)):
+                subJ = options[j]
+
+                duelIJ = countsDuels[subI][subJ]
+                duelJI = countsDuels[subJ][subI]
+
+                if duelIJ > duelJI:
+                    winsDuels[subI, subJ] = (duelIJ, duelJI)
+                elif duelIJ < duelJI:
+                    winsDuels[subJ, subI] = (duelJI, duelIJ)
+                else: #there is a tie, we use the timestamp as a tiebreaker
+                    timestampI = subI[2]
+                    timestampJ = subJ[2]
+
+                    if timestampI <= timestampJ:
+                        #the probability of having an equality on the timestamp is neglictible
+                        #the submission submitted earlier gets the priority
+                        winsDuels[subI, subJ] = (duelIJ, duelJI)
+        
+        #let's find the condorcet winner
+        nbWins = {c: 0 for c in options}
+        for (a, b) in winsDuels:
+            nbWins[a] += 1
+        
+        biggestWinner, nbWinsBigger = max(nbWins.items(), key=lambda x: x[1])
+        if nbWinsBigger == len(options) - 1: #the candidate won all its duels, it is an actual Condorcet winner
+            return biggestWinner, winsDuels
+        else:
+            return borda_elim(), winsDuels
